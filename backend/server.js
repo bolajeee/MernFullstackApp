fbbe120ed1fd166bd48b75f6bb8eeeea7ff41fb0
ibrahim O. Ibrahim <ibrahimomobolaji1999@gmail.com>
2024-09-16 21:57:22 +0100

    edit

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+// conventional method
+// const express = require{'express'}
+import express from "express";
+import dotenv  from "dotenv";
+import {connectDB} from "./config/db.js"
+
+
+const app = express();
+
+dotenv.config()
+
+app.get("/products", async (req, res) => {
+ const Product = res.body // user sent data
+ 
+    if (!Product.name || !Product.price || !Product.image) {
+     return res.status(400).json({sucess:false, message: "Please provide all fields"})
+    }
+    
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save()
+        res.status(201).json({sucess:true, data:newProduct})
+    } catch (error) {
+        console.error("Error in creating product", error.message)
+        res.status(500).json({success: false, message:"server error"})
+    }
+});
+
+app.listen(5000, () => {
+  connectDB()
+  console.log("server starts at http://localhost:5000");
+});
