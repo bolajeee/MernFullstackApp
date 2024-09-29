5fbb5bd4234e1459477fc9313d9d3a063fb382e2
ibrahim O. Ibrahim <ibrahimomobolaji1999@gmail.com>
2024-09-29 21:31:26 +0100

    edit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,19 @@
-import { mongoose } from "mongoose"
+import dotenv from "dotenv";
+dotenv.config();
 
-require("dotenv").config();
+console.log(process.env.MONGO_URI); 
 
-export const connectDB = async() => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            })
-        console.log(`MongoDB connected: ${conn.connection.host}`)
-    } catch(error) {
-        console.error(`Error: ${error.message}`)
-        process.exit(1) // process.exit code 1 means failure,while 0 means sucess
-    }
-}
+import mongoose from "mongoose"; 
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // process.exit code 1 means failure, while 0 means success
+  }
+};
